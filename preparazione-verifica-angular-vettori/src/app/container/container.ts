4bf297a48b2828f65286e96d5d4a873bb327d460
Alexandra Gumalal <gumalal.alexandra@iisgalvanimi.edu.it>
2026-03-27 10:01:57 +0000

    Punto 2 e 3 svolti

--- preparazione-verifica-angular-vettori/src/app/container/container.ts
+++ preparazione-verifica-angular-vettori/src/app/container/container.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   templateUrl: './container.html',
   styleUrl: './container.css',
 })
-export class Container {}
+export class Container {
+  // punto 2:lista vettore
+  macchine: string[] = [
+    'm1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm10'
+  ];
+}
